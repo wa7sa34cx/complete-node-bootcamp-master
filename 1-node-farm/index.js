dca1eb28a0b5da9fdffa35ccff030964775704ee
Alex Black <wa7@sa34.cx>
2021-02-21 20:43:41 +0300

    Start

---
+++ 1-node-farm/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+// const hello = 'Hello world!';
+// console.log(hello);
+
+// Blocking, syncronous way
+/*
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about avocado: ${textIn}\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written!')
+*/
+
+// Non-bocking, asyncronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('ERROR! 😱');
+
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+        console.log('You file has been written 😉');
+      })
+    });
+  });
+});
+console.log('Will read file!');
---
+++ 1-node-farm/web-server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  const pathName = req.url;
+
+  if (pathName === '/overview' || pathName === '/') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+
+
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requestd on port 8000...');
+});
