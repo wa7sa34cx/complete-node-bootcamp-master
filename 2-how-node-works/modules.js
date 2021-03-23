73b3e1e6fefe94978c834f1996dd6d5c73436778
Alex Black <wa7@sa34.cx>
2021-03-23 13:36:15 +0300

    Modules 2

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -1,3 +1,19 @@
-console.log(arguments);
+// console.log(arguments);
 
-console.log(require('module').wrapper);
+// console.log(require('module').wrapper);
+
+const Calculator = require('./test-module-1.js');
+// console.log(Calculator);
+
+// import { Calculator } from './test-module.js';
+const Calc = new Calculator();
+// console.log(Calc);
+
+console.log(Calc.add(3, 5));
+
+// --------
+
+// const calc2 = require('./test-module-2');
+const { add, multiply, divide } = require('./test-module-2');
+// console.log(calc2.add(4, 7));
+console.log(add(4, 7));
